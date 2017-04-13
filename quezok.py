22b56cb220269eed3bf26821340112fb213e1f69
akadeca <lxxxvi@openaliasbox.org>
2017-04-12 21:42:35 -0400

    Made the room "revealed" or not, and added looking.

--- quezok.py
+++ quezok.py
@@ -2,6 +2,8 @@
 # quezok.py - a text-based game
 
 class Brain:
+    """A Brain handles how an NPC responds to the player character."""
+
     def __init__(self, response='', retortance=''):
         self.response = response
         self.retortance = retortance
@@ -15,6 +17,8 @@ class Brain:
         print(self.retortance)
 
 class NPC:
+    """An NPC is a person or creature existing in the world."""
+
     def __init__(self, name, brain):
         self.name = name
         self.brain = brain
@@ -27,11 +31,18 @@ class NPC:
         """Retort the player's argument."""
         self.brain.retort()
 
+# Set up the world.
 npc = NPC("Jauld", Brain(response="What's up?", retortance="I disagree!"))
+revealed = False
 done = False
+
 while not done:
+    # Show what is around the user if it has not yet been revealed.
+    if not revealed:
+        print('You see ' + npc.name + ' here.')
+        revealed = True
+
     # Get the user's command as a lowercase string.
-    print('You see ' + npc.name + ' here.')
     cmd = raw_input('> ').lower()
 
     # Perform the command.
@@ -41,6 +52,12 @@ while not done:
     elif cmd == 'argue':
         # Argue with an NPC in the current zone.
         npc.retort()
+    elif cmd == 'look' or cmd == 'l':
+        # Make the current zone show itself again.
+        revealed = False
     elif cmd == 'quit' or cmd == 'q':
         # Quit the game.
         done = True
+    else:
+        # The user gave an unknown command.
+        print('Huh?')
